6e60a5a85fa05d96658abab3bd9b968a21750ffe
Dennis Akagha <d.akagha20@gmail.com>
2022-09-29 15:46:56 +0100

    Add project settup, CRUD functionality

---
+++ project/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect
+from .models import Project
+from .forms import ProjectForm
+
+
+# Read all project
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request, 'projects/projects.html', context)
+
+# Project detail
+def project(request, pk):
+    projObj = Project.objects.get(id=pk)
+    context = {'projObj': projObj}
+    return render(request, 'projects/single-project.html', context)
+
+# Create a Project
+def createProject(request):
+    form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
+
+# Update project
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
+
+
+# Delete project
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+
+    context = {'object': project}
+    return render(request, 'projects/delete_project.html', context)
